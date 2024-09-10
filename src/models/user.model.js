12f80f2b2d1cc60c2d358299415c84127613b339
Sumiran Bhawsar <bhawsarsumiran149@gmail.com>
2024-09-10 23:32:05 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,123 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/*
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    // Password ko hash karne ke liye salt generate karo
+    const salt = bcrypt.genSalt(10);
+    // Password ko hash karo aur replace kar do plain text password se
+    this.password = await bcrypt.hash(this.password, salt);
+    next(); // next middleware call karo
+  } catch (error) {
+    next(error); // agar error aata hai to middleware ko error pass karo
+  } 
+});
+*/
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  try {
+    const salt = bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+/*
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+*/
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  const user = this;
+  return await bcrypt.compare(password, user.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  const user = this;
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+      username: user.username,
+      fullName: user.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  )
+}
+userSchema.methods.generateRefreshToken = async function () {
+  const user = this;
+  return jwt.sign(
+    {
+      _id: user._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  )
+}
+
+export const User = mongoose.model("User", userSchema);
