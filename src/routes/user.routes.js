497362a2dd8c58d643c6df9a972e9f419be8d0c5
Sumiran Bhawsar <bhawsarsumiran149@gmail.com>
2024-09-16 23:44:24 +0530

    login user and auth test then how to log out user and delete the access and refresh token from browser and userfield

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import registerUser from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
@@ -16,6 +17,14 @@ userRouter.route("/register").post(
         }
     ]),
     registerUser
+)
+
+userRouter.route('/login').post(loginUser);
+
+// secured routes
+userRouter.route("/logout").post(
+    verifyJWT,
+    logoutUser
 )
 
 export default userRouter;
